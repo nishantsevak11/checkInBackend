835df007d82ca102d50590179c0748e4370bae0b
nishantsevak <nishantsevak7989@gmail.com>
2025-10-23 19:32:57 +0530

    cors restriction removed

--- src/app.js
+++ src/app.js
@@ -16,8 +16,9 @@ app.use(helmet());
 
 // CORS
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Body parser
